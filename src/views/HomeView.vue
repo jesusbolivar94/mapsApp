bcd6730161557f8d5ec48466d49093acea403218
Jesus Bolivar <j5rojo94@gmail.com>
2022-09-04 21:47:28 -0300

    Crear componente y css de caja de busqueda y resultados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
     <map-view/>
+    <searchBar/>
     <MyLocationBtn/>
 </template>
 
@@ -7,12 +8,14 @@
 import {defineComponent} from 'vue'
 import {usePlacesStore} from '@/composables'
 import MapView from '@/components/mapview/MapView.vue'
+import SearchBar from '@/components/searchbar/SearchBar.vue'
 import MyLocationBtn from '@/components/my-location-btn/MyLocationBtn.vue' // @ is an alias to /src
 
 export default defineComponent({
     name: 'HomeView',
     components: {
         MapView,
+        SearchBar,
         MyLocationBtn,
     },
     setup() {
