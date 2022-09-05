98b27dfe503d77310241605f0aa722bced04b740
Jesus Bolivar <j5rojo94@gmail.com>
2022-09-05 01:14:20 -0300

    Agrega marcadores a los places

--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -1,11 +1,40 @@
-import { MutationTree } from 'vuex';
-import { MapState } from './state';
+import {MutationTree} from 'vuex'
+import {MapState} from './state'
 import Mapboxgl from 'mapbox-gl'
+import {Feature} from '@/interfaces/places'
+import mapboxgl from 'mapbox-gl'
 
 const mutation: MutationTree<MapState> = {
-    setMap( state, map:Mapboxgl.Map ) {
+    setMap(state, map: Mapboxgl.Map) {
         state.map = map
+    },
+    setPlaceMarkers(state, places: Feature[]) {
+        // borrar marcadores
+        state.markers.forEach(marker => marker.remove())
+        state.markers = []
+
+        if (!state.map) return
+
+        // Crear los nuevos marcadores
+        for (const place of places) {
+            const [lng, lat] = place.center
+
+            const popup = new mapboxgl.Popup({offset: [0, 0]})
+                .setLngLat([lng, lat])
+                .setHTML(`
+                    <h4>${place.text}</h4>
+                    <p>${place.place_name}</p>
+                `)
+
+            const marker = new mapboxgl.Marker()
+                .setLngLat([lng, lat])
+                .setPopup(popup)
+                .addTo(state.map)
+
+            state.markers.push(marker)
+        }
+
     }
 }
 
-export default mutation;
+export default mutation
