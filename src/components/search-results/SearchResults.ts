e7b8ef214417b9b49d55679d30310ba196ec794e
Jesus Bolivar <j5rojo94@gmail.com>
2022-09-05 01:03:20 -0300

    Agrega interactividad para mover mapa al hacer clic en un place

--- src/components/search-results/SearchResults.ts
+++ src/components/search-results/SearchResults.ts
@@ -1,15 +1,29 @@
-import {defineComponent} from 'vue'
-import {usePlacesStore} from '@/composables'
+import {defineComponent, ref} from 'vue'
+import {useMapStore, usePlacesStore} from '@/composables'
+import {Feature} from '@/interfaces/places'
 
 export default defineComponent({
     name: 'SearchResults',
     setup() {
 
         const { places, isLoadingPlaces } = usePlacesStore()
+        const { map } = useMapStore()
+        const activePlace = ref('')
 
         return {
             places,
-            isLoadingPlaces
+            isLoadingPlaces,
+            activePlace,
+
+            onPlaceClick: ( place:Feature ) => {
+                activePlace.value = place.id
+                const [ lng, lat ] = place.center
+
+                map.value?.flyTo({
+                    zoom: 15,
+                    center: [ lng, lat ]
+                })
+            }
         }
     }
 })
--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -11,11 +11,16 @@
         <li
             v-for="place in places"
             :key="place.id"
-            class="list-group-item list-group-item-action text-left">
+            class="list-group-item list-group-item-action text-left"
+            :class="{ 'active': place.id === activePlace }"
+            @click="onPlaceClick( place )">
             <h5>{{place.text}}</h5>
             <p>{{place.place_name}}</p>
             <div class="text-end">
-                <button class="btn btn-outline-primary btn-sm">
+                <button
+                    class="btn btn-outline-primary btn-sm"
+                    :class="( place.id === activePlace ) ? 'btn-outline-light' : 'btn-outline-primary'"
+                >
                     Direcciones
                 </button>
             </div>
